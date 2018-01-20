1bca667f7fae8308be304a258c898efb9c4ca725
chertkovalex <chertkovalex@gmail.com>
2018-01-20 22:32:45 +0200

    Components Template & Styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<label>Enter Name:</label>
-    <input [(ngModel)]="name" placeholder="Enter Name" />
-    <h2>Welcome, {{name}}!</h2>`
+  templateUrl: `./app.component.html`,
+  styleUrls: [`./app.component.css`]
 })
 export class AppComponent {
   name = '';
